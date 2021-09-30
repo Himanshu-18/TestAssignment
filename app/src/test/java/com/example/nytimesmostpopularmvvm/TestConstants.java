40e26359e28897112e2d873cad0c40d221075bc6
Himanshu-18 <himanshu.rastogi@jetsynthesys.com>
2021-09-30 21:29:49 +0530

    code commmitted

---
+++ app/src/test/java/com/example/nytimesmostpopularmvvm/TestConstants.java
@@ -0,0 +1,9 @@
+package com.example.nytimesmostpopularmvvm;
+
+public interface TestConstants {
+    //variables
+//    String BASE_URL = "http://api.nytimes.com/svc/mostpopular/";
+
+    String BASE_URL="https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?";
+    String API_KEY = BuildConfig.API_KEY;
+}
